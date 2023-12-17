0c84ee54d02ac8aeb744ba130e6deaeb03052049
tryb3l <56679619+tryb3l@users.noreply.github.com>
2023-12-17 21:55:07 +0100

    updated basic configs, added URL for sample api tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,10 @@
 import { PlaywrightTestConfig } from "@playwright/test";
 
+export const baseURL = "https://reqres.in/api";
+
 const config: PlaywrightTestConfig = {
   //Directory where your tests are located
-  testDir: "./tests",
+  testDir: "tests/",
   // Time for each of your test to run from start to finish (60 seconds), increase if needed
   timeout: 60 * 1000,
   //It will fail your tests on CI if you have tests.only() or describe.only() in your tests
@@ -19,4 +21,38 @@ const config: PlaywrightTestConfig = {
   // Here you can choose which reporter to use
   // TODO: describe more reporter options
   reporter: "html",
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 10 * 1000,
+    ignoreHTTPSErrors: true,
+    video: "retain-on-failure",
+    screenshot: "off",
+  },
+  // Here you can specify which browsers you want to run your tests on
+  projects: [
+    {
+      name: "Chromium",
+      use: {
+        browserName: "chromium",
+        headless: true,
+      },
+    },
+    {
+      name: "Firefox",
+      use: {
+        browserName: "firefox",
+        headless: true,
+      },
+    },
+    {
+      name: "WebKit",
+      use: {
+        browserName: "webkit",
+        headless: true,
+      },
+    },
+  ],
 };
+
+export default config;
